d2bd7c267abb8ee45df08e8871ed16edc2eaeff5
Ahmed Elsabbahy <elsabbahyahmed@yahoo.com>
2017-03-29 19:28:20 -0400

    Add glide test

--- main.go
+++ main.go
@@ -1,10 +1,16 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+
+	"github.com/urfave/cli"
+)
 
 func hello(name string) string {
 	return fmt.Sprintf("Hello, %s!", name)
 }
 func main() {
+	cli.NewApp().Run(os.Args)
 	fmt.Println(hello("world"))
 }
