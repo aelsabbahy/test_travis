6d4642e6a1409e72a9d15fb4e7acf24ca32d6c18
Ahmed Elsabbahy <elsabbahyahmed@yahoo.com>
2017-04-07 16:52:02 -0400

    testing

--- main.go
+++ main.go
@@ -3,7 +3,9 @@ package main
 import (
 	"fmt"
 	"os"
+	"path/filepath"
 
+	homedir "github.com/mitchellh/go-homedir"
 	"github.com/urfave/cli"
 )
 
@@ -13,4 +15,18 @@ func hello(name string) string {
 func main() {
 	cli.NewApp().Run(os.Args)
 	fmt.Println(hello("world"))
+	h, err := homedir.Dir()
+	fmt.Println("homedir:", h, err)
+	if h, err := homedir.Dir(); err != nil {
+		fmt.Println("homedir:", h, err)
+	} else {
+		cwd, err := os.Getwd()
+		if err == nil {
+			homeDir := filepath.Join(cwd, ".glide")
+			fmt.Println("cwd:", homeDir, err)
+		} else {
+			homeDir := ".glide"
+			fmt.Println("else:", homeDir, err)
+		}
+	}
 }
